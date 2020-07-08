15a14db96279f1d231fc216bf3ff637c8fa7a928
Darian Tavana <dtavana@fb.com>
2020-07-08 15:12:15 -0400

    Start to implement avatar

--- app/src/main/java/com/dtavana/parstagram/activities/LoginActivity.java
+++ app/src/main/java/com/dtavana/parstagram/activities/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.dtavana.parstagram.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -15,14 +21,20 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
+import java.io.File;
+
+import static androidx.core.content.FileProvider.getUriForFile;
+
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = LoginActivity.class.getSimpleName();
 
+    ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         if(ParseUser.getCurrentUser() != null) {
--- app/src/main/java/com/dtavana/parstagram/activities/ProfileActivity.java
+++ app/src/main/java/com/dtavana/parstagram/activities/ProfileActivity.java
@@ -1,12 +1,19 @@
 package com.dtavana.parstagram.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 import com.dtavana.parstagram.R;
 import com.dtavana.parstagram.databinding.ActivityProfileBinding;
@@ -18,12 +25,20 @@ import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.Objects;
+
+import static androidx.core.content.FileProvider.getUriForFile;
 
 public class ProfileActivity extends AppCompatActivity {
 
     public static final String TAG = ProfileActivity.class.getSimpleName();
+
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
 
     ActivityProfileBinding binding;
 
@@ -39,11 +54,20 @@ public class ProfileActivity extends AppCompatActivity {
         binding.tvUsername.setText(ParseUser.getCurrentUser().getUsername());
         ParseFile avatar = User.getAvatar();
         if(avatar != null) {
+            Log.i(TAG, "onCreate: Avatar URL: " + avatar.getUrl());
             GlideApp
                     .with(this)
                     .load(avatar.getUrl())
+                    .override(150, 150)
                     .into(binding.ivAvatar);
         }
+
+        binding.btnSetAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
 
         binding.btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +82,65 @@ public class ProfileActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG, "onActivityResult: Succesfully took avatar");
+                User.setAvatar(new ParseFile(photoFile));
+                ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Log.i(TAG, "done: Succesfully saved new avatar");
+                        Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                        // RESIZE BITMAP, see section below
+                        // Load the taken image into a preview
+                        binding.ivAvatar.setImageBitmap(takenImage);
+                    }
+                });
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = getUriForFile(this, "com.codepath.fileprovider.dtavana-parstagram", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    private File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
     }
 }
